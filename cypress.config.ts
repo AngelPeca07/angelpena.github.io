a125cb3af7f9281c1b28eccd8a9b9e0a94963d18
Angel Peña <jose.angpc@gmail.com>
2026-01-30 12:03:30 -0600

    rules were added to cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
   },
 });
